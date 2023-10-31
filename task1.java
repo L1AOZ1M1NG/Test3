2575a6cb3a29a8c3d300589066361280e0778482
L1AOZ1M1NG <135006814+L1AOZ1M1NG@users.noreply.github.com>
2023-10-31 19:28:20 +0800

    Create task1.java
    The initial version of the code.

---
+++ task1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class task1{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Please tell me the start:");
+        int start = input.nextInt();
+        System.out.print("Please tell me the end:");
+        int end  = input.nextInt();
+        int sum = 0;
+        do{
+            sum += start++;
+
+
+        }while (start <= end);
+
+        System.out.printf("The result is %d%n",sum);
+
+
+
+    }
+}
