d694bf28b4ecbfb09a7532242b1439b59e5d51c2
L1AOZ1M1NG <135006814+L1AOZ1M1NG@users.noreply.github.com>
2023-10-31 19:31:12 +0800

    Update task1.java
    I change the file in the branch1

--- task1.java
+++ task1.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 public class task1{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        
         System.out.print("Please tell me the start:");
         int start = input.nextInt();
         System.out.print("Please tell me the end:");
-        int end  = input.nextInt();
-        int sum = 0;
-        do{
-            sum += start++;
+        int end = input.nextInt();
 
-
-        }while (start <= end);
-
-        System.out.printf("The result is %d%n",sum);
-
-
-
+        if (start >= end){
+            System.out.println("Errors occur!");
+        
+        }
+        else{
+            int sum = 0;
+            do{
+                sum += start++;
+            }while (start <= end);
+            System.out.printf("The result is %d%n",sum);
+        }
     }
 }
